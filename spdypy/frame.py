030ff64db0a0e278ae9cf9f74112017bad4b96b5
Cory Benfield <lukasaoz@gmail.com>
2013-09-30 19:36:40 +0100

    Control frame types.

--- spdypy/frame.py
+++ spdypy/frame.py
@@ -5,6 +5,15 @@ spdypy.frame
 
 Defines SPDYPy's internal representation of a SPDY frame.
 """
+# Define our control frame types.
+SYN_STREAM = 1
+SYN_REPLY = 2
+RST_STREAM = 3
+SETTINGS = 4
+PING = 6
+GOAWAY = 7
+HEADERS = 8
+WINDOW_UPDATE = 9
 
 
 class Frame(object):
