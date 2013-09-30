5d961137d764dd26b1e893946a39d475f9de14ea
Cory Benfield <lukasaoz@gmail.com>
2013-09-30 18:34:52 +0100

    Begin API testing.

---
+++ test/test_api.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+tests/api
+~~~~~~~~~
+
+Tests of the top-level SPDYPy API. These will be relatively sparse for the
+moment.
+"""
+# Nasty little path hack.
+import sys
+sys.path.append('.')
+
+
+class TestAPI(object):
+    """
+    Tests for the top-level spdypy API.
+    """
+    def test_can_import_spdypy_on_py_33(self):
+        import spdypy
+        assert True
