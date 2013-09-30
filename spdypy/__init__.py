11a09b66aad7a3aaaaad951854ac5ce2ba4b0376
Cory Benfield <lukasaoz@gmail.com>
2013-09-30 18:26:26 +0100

    Basic spdypy module.

---
+++ spdypy/__init__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+spdypy - SPDY for lovers
+~~~~~~~~~~~~~~~~~~~~~~~~
+
+SPDYPy is a library for easily making and working with SPDY connections,
+written in pure Python, depending on nothing but the standard library.
+
+:copyright: (c) 2013 by Cory Benfield.
+:license: MIT, see LICENSE for details.
+"""
+
+__title__ = 'spdypy'
+__version__ = '0.0.0'
+__build__ = 0x000000
+__author__ = 'Cory Benfield'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2013 Cory Benfield'
+
+# Python version check.
+import sys
+
+version = sys.version_info
+
+if version[0] == 2 or version[1] < 3:
+    raise ImportError("Minimum Python version is 3.3.")
