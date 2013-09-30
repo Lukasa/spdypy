54331ca67f2aa2362ab166c88b7de4f50c9291d7
Cory Benfield <lukasaoz@gmail.com>
2013-09-30 19:25:18 +0100

    Frame default stuff.

--- spdypy/frame.py
+++ spdypy/frame.py
@@ -6,8 +6,14 @@ spdypy.frame
 Defines SPDYPy's internal representation of a SPDY frame.
 """
 
+
 class Frame(object):
     """
     A single SPDY frame.
     """
-    pass
+    def __init__(self):
+        self.control = None
+        self.version = None
+        self.type = None
+        self.flags = []
+        self.data = None
--- test/test_frame.py
+++ test/test_frame.py
@@ -10,3 +10,11 @@ from spdypy.frame import Frame
 class TestFrame(object):
     def test_can_create_blank_frame(self):
         assert Frame()
+
+    def test_frame_default_field_values(self):
+        fr = Frame()
+        assert fr.control is None
+        assert fr.version is None
+        assert fr.type is None
+        assert fr.flags == []
+        assert fr.data is None
