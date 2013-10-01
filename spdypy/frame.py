43f39c16370ffb5ca081635f6ee0b84649919721
Cory Benfield <lukasaoz@gmail.com>
2013-10-01 18:47:01 +0100

    Add some flags handling.

--- spdypy/frame.py
+++ spdypy/frame.py
@@ -15,6 +15,39 @@ GOAWAY = 7
 HEADERS = 8
 WINDOW_UPDATE = 9
 
+# Define the flags we know about.
+FLAG_FIN = 'FLAG_FIN'
+FLAG_UNIDIRECTIONAL = 'FLAG_UNIDIRECTIONAL'
+FLAG_CLEAR_SETTINGS = 'FLAG_CLEAR_SETTINGS'
+
+
+def flags(byte, frame_type=None):
+    """
+    Given the flag byte and the frame type, return the flags that have been
+    set. If the frame type is not set, assumes a data frame.
+
+    :param byte: The flag byte.
+    :param frame_type: The control frame type, or None if the frame is a data
+                       frame.
+    """
+    flags = []
+
+    if frame_type in (None, SYN_REPLY, HEADERS):
+        if byte & 0x01:
+            flags.append(FLAG_FIN)
+
+    elif frame_type == SYN_STREAM:
+        if byte & 0x01:
+            flags.append(FLAG_FIN)
+        if byte & 0x02:
+            flags.append(FLAG_UNIDIRECTIONAL)
+
+    elif frame_type == SETTINGS:
+        if byte & 0x01:
+            flags.append(FLAG_CLEAR_SETTINGS)
+
+    return flags
+
 
 class Frame(object):
     """
--- test/test_frame.py
+++ test/test_frame.py
@@ -4,7 +4,9 @@ test/test_frame
 ~~~~~~~~~~~~~~~
 Tests of the SPDY frame.
 """
-from spdypy.frame import Frame
+from spdypy.frame import (Frame, flags, SYN_STREAM, SYN_REPLY, RST_STREAM,
+                          SETTINGS, PING, GOAWAY, HEADERS, WINDOW_UPDATE,
+                          FLAG_FIN, FLAG_UNIDIRECTIONAL, FLAG_CLEAR_SETTINGS)
 
 
 class TestFrame(object):
@@ -19,3 +21,19 @@ class TestFrame(object):
         assert fr.flags == []
         assert fr.data is None
         assert fr.stream_id is None
+
+class TestFlags(object):
+    def test_all_flags_set(self):
+        # This test isn't totally realistic, as the server shouldn't set
+        # undefined flags, but it's a good test of output.
+        expected = {SYN_STREAM: [FLAG_FIN, FLAG_UNIDIRECTIONAL],
+                    SYN_REPLY: [FLAG_FIN],
+                    RST_STREAM: [],
+                    SETTINGS: [FLAG_CLEAR_SETTINGS],
+                    PING: [],
+                    GOAWAY: [],
+                    HEADERS: [FLAG_FIN],
+                    WINDOW_UPDATE: []}
+
+        for frame_type, result in expected.items():
+            assert result == flags(0xFF, frame_type)
