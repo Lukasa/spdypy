788d9f9894e7aa4e69443c35a4d7567a0dca8ef8
Cory Benfield <lukasaoz@gmail.com>
2013-10-09 19:13:37 +0100

    SSLContext!

--- spdypy/connection.py
+++ spdypy/connection.py
@@ -5,6 +5,9 @@ spdypy.connection
 
 Contains the code necessary for working with SPDY connections.
 """
+import ssl
+
+
 # Define some states for SPDYConnections.
 NEW = 'NEW'
 
@@ -22,6 +25,11 @@ class SPDYConnection(object):
     def __init__(self, host):
         self.host = host
         self._state = NEW
+        self.__context = ssl.SSLContext(ssl.PROTOCOL_SSLv23)
+
+        # Set up the initial SSL context.
+        self.__context.set_default_verify_paths()
+        self.__context.set_npn_protocols(['http/1.1', 'spdy/3.1'])
 
     def request(self, method, url, body=None, headers={}):
         """
