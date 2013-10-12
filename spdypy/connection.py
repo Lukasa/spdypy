1127e6ca9e50fed955ab458746950b9fe5e826b3
Cory Benfield <lukasaoz@gmail.com>
2013-10-12 09:15:05 +0100

    Some structural code: not relevant right now.

--- spdypy/connection.py
+++ spdypy/connection.py
@@ -25,11 +25,12 @@ class SPDYConnection(object):
     def __init__(self, host):
         self.host = host
         self._state = NEW
-        self.__context = ssl.SSLContext(ssl.PROTOCOL_SSLv23)
+        self._context = ssl.SSLContext(ssl.PROTOCOL_SSLv23)
+        self._sck = None
 
         # Set up the initial SSL context.
-        self.__context.set_default_verify_paths()
-        self.__context.set_npn_protocols(['http/1.1', 'spdy/3.1'])
+        self._context.set_default_verify_paths()
+        self._context.set_npn_protocols(['http/1.1', 'spdy/3', 'spdy/3.1'])
 
     def request(self, method, url, body=None, headers={}):
         """
@@ -44,3 +45,13 @@ class SPDYConnection(object):
         send with the request.
         """
         pass
+
+    def _connect(self):
+        """
+        This method will open a socket connection to the remote server and
+        perform the SSL handshake necessary to open a SPDY connection. This
+        method is a no-op if there is already an open socket, so it should be
+        safe to call in all circumstances.
+        """
+        if self._sck is not None:
+            return
