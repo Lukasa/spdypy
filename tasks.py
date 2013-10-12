26d5489eac4647438fdc371d3cfae20288a337f5
Cory Benfield <lukasaoz@gmail.com>
2013-10-12 10:06:24 +0100

    Better coverage reports.

--- tasks.py
+++ tasks.py
@@ -3,4 +3,4 @@ from invoke import task, run
 
 @task
 def test():
-    run('py.test --cov-report term --cov spdypy test/', pty=True)
+    run('py.test --cov-report term-missing --cov spdypy test/', pty=True)
