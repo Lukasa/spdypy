dcae151c8987d034f742ec735921a94d0882549c
Cory Benfield <lukasaoz@gmail.com>
2013-10-19 18:44:55 +0100

    Basic example, not very good.

---
+++ examples/basic.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+A basic example of how to use SPDY.
+
+This example is currently incomplete, but should remain current to the state of
+the library. This means that it enumerates the full state of everything
+SPDYPy can do.
+"""
+import sys
+sys.path.append('.')
+import spdypy
+
+conn = spdypy.SPDYConnection('www.google.com')
+conn.putrequest(b'GET', b'/')
+conn.putheader(b'user-agent', b'spdypy')
+conn.putheader(b'accept', b'*/*')
+conn.putheader(b'accept-encoding', b'gzip,deflate')
+conn.endheaders()
+
+# Debugging output for now.
+frame_list = conn._read_outstanding()
+while frame_list:
+    for frame in frame_list:
+        print(frame)
+
+    frame_list = conn._read_outstanding()
