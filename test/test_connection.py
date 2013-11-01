1e91b4ad94dd4a986adce22350cec8bd24fa4865
Cory Benfield <lukasaoz@gmail.com>
2013-11-01 09:22:07 +0000

    Test correctly incrementing stream IDs.

--- test/test_connection.py
+++ test/test_connection.py
@@ -7,6 +7,7 @@ Tests for the SPDYConnection object.
 """
 import spdypy
 import spdypy.connection
+from .test_stream import MockConnection
 
 
 class TestSPDYConnection(object):
@@ -23,3 +24,18 @@ class TestSPDYConnectionState(object):
     def test_initial_connection_state_is_new(self):
         conn = spdypy.SPDYConnection(None)
         assert conn._state == spdypy.connection.NEW
+
+    def test_new_streams_use_new_stream_id(self):
+        conn = spdypy.SPDYConnection('www.google.com')
+        conn._sck = MockConnection()
+        stream_id = conn.putrequest(b'GET', b'/')
+
+        assert len(conn._streams) == 1
+        assert stream_id == 1
+        assert conn._streams[stream_id]
+
+        second_stream_id = conn.putrequest(b'POST', b'other')
+
+        assert len(conn._streams) == 2
+        assert second_stream_id == 3
+        assert conn._streams[second_stream_id]
